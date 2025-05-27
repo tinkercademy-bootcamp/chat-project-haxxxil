8b377bd2522a0f92e5c91941fefd4f27deb03e5a
Harshil Singh <hars.sin.hil@gmail.com>
2025-05-27 06:28:12 +0000

    refaction client get server

--- exercise-2/exercise-1/src/tcp_echo_client.cc
+++ exercise-2/exercise-1/src/tcp_echo_client.cc
@@ -17,22 +17,28 @@ int create_socket()
   return my_sock;
 }
 
+sockaddr_in get_server_addr(std::string serverAddr, int serverPort)
+{
+  sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_port = htons(serverPort);
+  // Convert IPv4 and IPv6 addresses from text to binary form
+  if (inet_pton(AF_INET, serverAddr.c_str(), &address.sin_addr) <= 0) {
+    std::cerr << "Invalid address/ Address not supported\n";
+    exit(EXIT_FAILURE);
+  }
+  return address;
+}
+
 int main() {
   // #Question - are these the same type?
   std::string message = "Hello from client";
   const int kPort = 8080;
   const std::string kServerAddress = "127.0.0.1";
-  sockaddr_in address;
   const int kBufferSize = 1024;
   char buffer[kBufferSize] = {0};
   int my_sock = create_socket();
-  address.sin_family = AF_INET;
-  address.sin_port = htons(kPort);
-  // Convert IPv4 and IPv6 addresses from text to binary form
-  if (inet_pton(AF_INET, kServerAddress.c_str(), &address.sin_addr) <= 0) {
-    std::cerr << "Invalid address/ Address not supported\n";
-    return -1;
-  }
+  sockaddr_in address = get_server_addr(kServerAddress, kPort);
   // Connect to the server
   if (connect(my_sock, (sockaddr *)&address, sizeof(address)) < 0) {
     std::cerr << "Connection Failed\n";
