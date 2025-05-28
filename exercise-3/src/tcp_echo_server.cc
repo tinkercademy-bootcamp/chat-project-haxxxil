af8d250473cabe2d1d7adedf6fb4eeeb683550dc
Harshil Singh <hars.sin.hil@gmail.com>
2025-05-28 02:07:27 +0000

    add check_error in client and update create_socket

--- exercise-3/src/tcp_echo_server.cc
+++ exercise-3/src/tcp_echo_server.cc
@@ -6,12 +6,17 @@
 
 const int kBufferSize = 1024;
 
-int create_socket() {
-  int my_sock;
-  if ((my_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
-    std::cerr << "Socket creation error\n";
+void check_error(bool test, std::string error_message) {
+  if (test) {
+    std::cerr << error_message << "\n";
     exit(EXIT_FAILURE);
   }
+}
+
+int create_socket() {
+  int my_sock;
+  my_sock = socket(AF_INET, SOCK_STREAM, 0)
+  check_error(my_sock<0, "Socket creation error\n");
   return my_sock;
 }
 
