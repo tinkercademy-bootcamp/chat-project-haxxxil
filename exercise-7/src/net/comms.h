88432686cf3566ee7e94ff61b809b70615a1f85d
Harshil Singh <hars.sin.hil@gmail.com>
2025-06-03 11:45:18 +0000

    Exercise-7 : Add Command class
    This class would be used by both the server and client to send requests
    and would also be responsible for translating the data into a string
    sent over the network and vice-versa.

--- exercise-7/src/net/comms.h
+++ exercise-7/src/net/comms.h
@@ -4,8 +4,31 @@
 #include <string>
 
 namespace tt::chat::comms {
-  std::string read_from_socket();
-  int write_to_socket(char * message, int msg_size);
+
+std::string read_from_socket();
+int write_to_socket(char * message, int msg_size);
+
+class Command {
+  public:
+    enum CommandType {
+      SEND_MSG,
+      READ_MSG,
+      CHANGE_CHAN,
+      LIST_CHAN,
+      CREATE_CHAN
+    };
+
+    CommandType cmd;
+    std::string user;
+    std::string cmd_request;
+    std::string message;
+
+    char * get_string_ptr();
+    unsigned get_string_size();
+    bool createCommand(std::string & data);
+    bool createString();
+};
+
 }
 
 #endif
